f78131408973cb82782cf3c8f404f2e5ecc837c2
Sergera60 <112910302+Sergera60@users.noreply.github.com>
2024-12-10 20:54:37 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/bouslama_mohamedaziz_4bi4/BouslamaMohamedaziz4bi4Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.bouslama_mohamedaziz_4bi4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouslamaMohamedaziz4bi4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BouslamaMohamedaziz4bi4Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/bouslama_mohamedaziz_4bi4/BouslamaMohamedaziz4bi4ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.bouslama_mohamedaziz_4bi4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouslamaMohamedaziz4bi4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
